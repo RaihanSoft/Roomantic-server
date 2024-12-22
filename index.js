519498458a19771d9d4738672ba1b65e1501addb
Raihan <aboraihan14@gmail.com>
2024-12-23 01:02:09 +0600

    Route to get room details by ID

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
 
-
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khjiv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -33,43 +30,30 @@ async function run() {
         const hotelCollection = client.db("hotel-booking").collection("rooms");
         const bookingsCollection = client.db("hotel-booking").collection("bookings");
 
-        // ! 1. GET - Fetch all rooms
-
         // Route to get all rooms
         app.get('/rooms', async (req, res) => {
+            const { minPrice, maxPrice } = req.query;
+            const query = {};
+            if (minPrice) query.price = { $gte: parseFloat(minPrice) };
+            if (maxPrice) query.price = { ...query.price, $lte: parseFloat(maxPrice) };
+
             try {
-                const hotelCollection = client.db("hotel-booking").collection('rooms'); // Access the 'rooms' collection
-                const rooms = await hotelCollection.find().toArray(); // Fetch all rooms
-                res.json(rooms); // Send the rooms data as a JSON response
+                const rooms = await hotelCollection.find(query).toArray();
+                res.json(rooms);
             } catch (error) {
                 res.status(500).send('Error fetching rooms');
             }
         });
 
-
-
-
-
-        //!  Route to get bookings for a specific u  ser by email
-        app.get('/myBookings', async (req, res) => {
-            const { email } = req.query;
-            if (!email) {
-                return res.status(400).json({ message: "Email query parameter is required." });
-            }
-
-            try {
-                const bookings = await bookingsCollection.find({ userEmail: email }).toArray();
-                res.status(200).json(bookings);
-            } catch (error) {
-                console.error("Error fetching bookings:", error);
-                res.status(500).json({ message: "Error fetching bookings." });
-            }
+        // Route to get room details by ID
+        app.get('/rooms/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: new ObjectId(id) };
+            const result = await hotelCollection.findOne(query);
+            res.json(result);
         });
 
-
-
-
-        
+       
 
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
